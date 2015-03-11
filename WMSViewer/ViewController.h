d4d3eb6a1faebd1454dbeffac794fab5dfa4b156
Alan Steremberg <alan.steremberg@gsa.gov>
2015-03-11 10:56:41 -0400

    Initial Commit

---
+++ WMSViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMSViewer
+//
+//  Created by Alan Steremberg on 3/11/15.
+//  Copyright (c) 2015 Alan Steremberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WMSViewer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WMSViewer
+//
+//  Created by Alan Steremberg on 3/11/15.
+//  Copyright (c) 2015 Alan Steremberg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
